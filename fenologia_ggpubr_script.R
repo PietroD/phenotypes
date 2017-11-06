db961276fb88a7aad712b9f5a13653919e248619
Pietro Delfino <delfino.pietro@gmail.com>
2017-11-06 16:21:19 +0100

    Added line in second position

--- fenologia_ggpubr_script.R
+++ fenologia_ggpubr_script.R
@@ -1,4 +1,5 @@
 head(fenologia)
+str(fenologia)
 
 ggplot(fenologia$VB, aes(as.factor(YEAR), value)) + 
   geom_violin(aes(colour=variable)) + geom_jitter(aes(colour=variable), alpha=0.3,width = 0.25) + ggtitle("VB x Year") + 
